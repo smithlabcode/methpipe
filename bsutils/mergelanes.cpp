ab4827e922bac46faec6e49a50cd3b8269522bf9
songqiang <qiang.song@usc.edu>
2010-01-11 17:41:19 +0000

    re-indentation

--- bsutils/mergelanes.cpp
+++ bsutils/mergelanes.cpp
@@ -58,46 +58,46 @@ using std::ofstream;
 template <class T>
 class FileIterator {
 public:
-  FileIterator(const std::string f, const size_t bs);
-  void increment_first() {
-    if (++first == buffer.end()) {
-      refill_buffer();
-    }
-  }
-  typename std::vector<T>::const_iterator get_first() const {return first;}
-  bool first_is_good() const {return (!in.eof() || first < buffer.end());}
+		FileIterator(const std::string f, const size_t bs);
+		void increment_first() {
+				if (++first == buffer.end()) {
+						refill_buffer();
+				}
+		}
+		typename std::vector<T>::const_iterator get_first() const {return first;}
+		bool first_is_good() const {return (!in.eof() || first < buffer.end());}
   
 private:
-  std::ifstream in;
-  std::vector<T> buffer;
-  typename std::vector<T>::iterator first;
-  void refill_buffer();
+		std::ifstream in;
+		std::vector<T> buffer;
+		typename std::vector<T>::iterator first;
+		void refill_buffer();
 };
 
 std::istream& 
 operator>>(std::istream &in, GenomicRegion &region){
-  static const size_t buffer_size = 10000; // Magic
-  char buffer[buffer_size];
-  in.getline(buffer, buffer_size);
-  region = GenomicRegion(buffer);
-  return in;
+		static const size_t buffer_size = 10000; // Magic
+		char buffer[buffer_size];
+		in.getline(buffer, buffer_size);
+		region = GenomicRegion(buffer);
+		return in;
 }
 
 /*  THIS FUNCTION FILLS A BUFFER FOR GenomicRegion OBJECTS
  */
 void 
 fill_buffer(std::ifstream &in, const size_t buffer_start, 
-	    vector<GenomicRegion> &buffer) {
-  GenomicRegion tmp;
-  size_t i = buffer_start;
-  assert(buffer_start <= buffer.size());
-  for (; i != buffer.size() && !in.eof(); ++i) {
-    in >> tmp;
-    buffer[i].swap(tmp);
-    in.peek();
-  }
-  if (i < buffer.size())
-    buffer.erase(buffer.begin() + i, buffer.end());
+			vector<GenomicRegion> &buffer) {
+		GenomicRegion tmp;
+		size_t i = buffer_start;
+		assert(buffer_start <= buffer.size());
+		for (; i != buffer.size() && !in.eof(); ++i) {
+				in >> tmp;
+				buffer[i].swap(tmp);
+				in.peek();
+		}
+		if (i < buffer.size())
+				buffer.erase(buffer.begin() + i, buffer.end());
 }
 
 /*  THIS FUNCTION FILLS A BUFFER FOR THE ACTUAL READS, REPRESENTED AS
@@ -105,19 +105,19 @@ fill_buffer(std::ifstream &in, const size_t buffer_start,
  */
 void 
 fill_buffer(std::ifstream &in, const size_t buffer_start, 
-	    vector<string> &buffer) {
-  string tmp;
-  size_t i = buffer_start;
-  for (; i != buffer.size() && !in.eof(); ++i) {
-    // the read name...
-    in >> tmp; // DANGER: assumes that the name of the read has no
-	       // spaces in it!!
-    // the read itself:
-    in >> buffer[i];
-    in.peek();
-  }
-  if (i < buffer.size())
-    buffer.erase(buffer.begin() + i, buffer.end());
+			vector<string> &buffer) {
+		string tmp;
+		size_t i = buffer_start;
+		for (; i != buffer.size() && !in.eof(); ++i) {
+				// the read name...
+				in >> tmp; // DANGER: assumes that the name of the read has no
+				// spaces in it!!
+				// the read itself:
+				in >> buffer[i];
+				in.peek();
+		}
+		if (i < buffer.size())
+				buffer.erase(buffer.begin() + i, buffer.end());
 }
 
 
@@ -125,42 +125,42 @@ fill_buffer(std::ifstream &in, const size_t buffer_start,
  *  STRINGS, AND MUST BE IN A FASTA FORMAT FILE
  */
 struct FASTQRecord {
-  FASTQRecord() {}
-  FASTQRecord(const FASTQRecord &rhs) : 
-    name(rhs.name), seq(rhs.seq), qual(rhs.qual) {}
-  FASTQRecord(const string &n_,
-	      const string &s_,
-	      const string &q_) : name(n_), seq(s_), qual(q_) {}
-  string name;
-  string seq;
-  string qual;
+		FASTQRecord() {}
+		FASTQRecord(const FASTQRecord &rhs) : 
+				name(rhs.name), seq(rhs.seq), qual(rhs.qual) {}
+		FASTQRecord(const string &n_,
+					const string &s_,
+					const string &q_) : name(n_), seq(s_), qual(q_) {}
+		string name;
+		string seq;
+		string qual;
 };
 
 std::ostream&
 operator<<(std::ostream &s, const FASTQRecord &rhs) {
-  return s << rhs.name << '\n'
-	   << rhs.seq << "\n+\n"
-	   << rhs.qual;
+		return s << rhs.name << '\n'
+				 << rhs.seq << "\n+\n"
+				 << rhs.qual;
 }
 
 void 
 fill_buffer(std::ifstream &in, const size_t buffer_start, 
-	    vector<FASTQRecord> &buffer) {
-  string tmp, read_name, read_seq, scores_seq;
-  size_t i = buffer_start;
-  for (; i != buffer.size() && !in.eof(); ++i) {
-    // the read name...
-    in >> read_name; // DANGER: assumes that the name of the read has no
-	       // spaces in it!!
-    // the read itself:
-    in >> read_seq;
-    in >> tmp;
-    in >> scores_seq;
-    buffer[i] = FASTQRecord(read_name, read_seq, scores_seq);
-    in.peek();
-  }
-  if (i < buffer.size())
-    buffer.erase(buffer.begin() + i, buffer.end());
+			vector<FASTQRecord> &buffer) {
+		string tmp, read_name, read_seq, scores_seq;
+		size_t i = buffer_start;
+		for (; i != buffer.size() && !in.eof(); ++i) {
+				// the read name...
+				in >> read_name; // DANGER: assumes that the name of the read has no
+				// spaces in it!!
+				// the read itself:
+				in >> read_seq;
+				in >> tmp;
+				in >> scores_seq;
+				buffer[i] = FASTQRecord(read_name, read_seq, scores_seq);
+				in.peek();
+		}
+		if (i < buffer.size())
+				buffer.erase(buffer.begin() + i, buffer.end());
 }
 
 
@@ -169,160 +169,160 @@ fill_buffer(std::ifstream &in, const size_t buffer_start,
    BUFFER. HOPEFULLY THE FIRST ONE WILL NOT HIT THE END BEFORE THE
    LAST: THE FIRST IS ALWAYS SUPPOSED TO BE LESS THAN OR EQUAL TO THE
    LAST.
- */
+*/
 template <class T> void
 FileIterator<T>::refill_buffer() {
-  first = buffer.begin();
-  fill_buffer(in, 0, buffer);
+		first = buffer.begin();
+		fill_buffer(in, 0, buffer);
 }
 
 template <class T>
 FileIterator<T>::FileIterator(const std::string f, const size_t bs) :
-  buffer(std::vector<T>(bs)) {
-  in.open(f.c_str());
-  if (!in) throw RMAPException("cannot open input file " + f);
-  fill_buffer(in, 0, buffer);
-  first = buffer.begin();
+		buffer(std::vector<T>(bs)) {
+		in.open(f.c_str());
+		if (!in) throw RMAPException("cannot open input file " + f);
+		fill_buffer(in, 0, buffer);
+		first = buffer.begin();
 }
 
 struct ComparePairs {
-  bool operator()(const pair<GenomicRegion, size_t> &a,
-		  const pair<GenomicRegion, size_t> &b) const {
-    return !(a.first < b.first);
-  }
+		bool operator()(const pair<GenomicRegion, size_t> &a,
+						const pair<GenomicRegion, size_t> &b) const {
+				return !(a.first < b.first);
+		}
 };
 
 
 int 
 main(int argc, const char **argv) {
 
-  try {
+		try {
 
-    bool VERBOSE = false;
-    string map_outfile;
-    string read_outfile;
-    size_t random_number_seed = numeric_limits<size_t>::max();
+				bool VERBOSE = false;
+				string map_outfile;
+				string read_outfile;
+				size_t random_number_seed = numeric_limits<size_t>::max();
     
-    size_t BUFFER_SIZE = 10000ul;
+				size_t BUFFER_SIZE = 10000ul;
     
-    /****************** COMMAND LINE OPTIONS ********************/
-    OptionParser opt_parse("mergelanes", 
-			   "A program to merge different lanes in a shotgun bisulfite "
-			   "sequencing experiment producing one large sorted file of all read "
-			   "mapping locations (without duplicate 5' ends) and one large file "
-			   "sorted similarly containing the corresponding sequences.",
-			   "<fasta-reads-file-1> [<fasta-reads-file-2> ...]");
-    opt_parse.add_opt("output", 'o', "Name of maps output file", 
-		      true, map_outfile);
-    opt_parse.add_opt("readout", 'r', "Name of reads output file", 
-		      true, read_outfile);
-    opt_parse.add_opt("verbose", 'v', "print more run info", false, VERBOSE);
-    vector<string> leftover_args;
-    opt_parse.parse(argc, argv, leftover_args);
-    if (argc == 1 || opt_parse.help_requested()) {
-      cerr << opt_parse.help_message() << endl
-	   << opt_parse.about_message() << endl;
-      return EXIT_SUCCESS;
-    }
-    if (opt_parse.about_requested()) {
-      cerr << opt_parse.about_message() << endl;
-      return EXIT_SUCCESS;
-    }
-    if (opt_parse.option_missing()) {
-      cerr << opt_parse.option_missing_message() << endl;
-      return EXIT_SUCCESS;
-    }
-    if (leftover_args.empty()) {
-      cerr << opt_parse.help_message() << endl;
-      return EXIT_SUCCESS;
-    }
-    const string mapped_file_names_file(leftover_args.front());
-    vector<string> mapped_files;
-    if (!mapped_file_names_file.empty())
-      read_filename_file(mapped_file_names_file.c_str(), mapped_files);
-    else {
-      cerr << opt_parse.help_message() << endl;
-      return EXIT_SUCCESS;
-    }
-    const string read_file_names_file(leftover_args.back());
-    vector<string> reads_files;
-    if (!read_file_names_file.empty())
-      read_filename_file(read_file_names_file.c_str(), reads_files);
-    else {
-      cerr << opt_parse.help_message() << endl;
-      return EXIT_SUCCESS;
-    }
-    /****************** END COMMAND LINE OPTIONS *****************/
+				/****************** COMMAND LINE OPTIONS ********************/
+				OptionParser opt_parse("mergelanes", 
+									   "A program to merge different lanes in a shotgun bisulfite "
+									   "sequencing experiment producing one large sorted file of all read "
+									   "mapping locations (without duplicate 5' ends) and one large file "
+									   "sorted similarly containing the corresponding sequences.",
+									   "<fasta-reads-file-1> [<fasta-reads-file-2> ...]");
+				opt_parse.add_opt("output", 'o', "Name of maps output file", 
+								  true, map_outfile);
+				opt_parse.add_opt("readout", 'r', "Name of reads output file", 
+								  true, read_outfile);
+				opt_parse.add_opt("verbose", 'v', "print more run info", false, VERBOSE);
+				vector<string> leftover_args;
+				opt_parse.parse(argc, argv, leftover_args);
+				if (argc == 1 || opt_parse.help_requested()) {
+						cerr << opt_parse.help_message() << endl
+							 << opt_parse.about_message() << endl;
+						return EXIT_SUCCESS;
+				}
+				if (opt_parse.about_requested()) {
+						cerr << opt_parse.about_message() << endl;
+						return EXIT_SUCCESS;
+				}
+				if (opt_parse.option_missing()) {
+						cerr << opt_parse.option_missing_message() << endl;
+						return EXIT_SUCCESS;
+				}
+				if (leftover_args.empty()) {
+						cerr << opt_parse.help_message() << endl;
+						return EXIT_SUCCESS;
+				}
+				const string mapped_file_names_file(leftover_args.front());
+				vector<string> mapped_files;
+				if (!mapped_file_names_file.empty())
+						read_filename_file(mapped_file_names_file.c_str(), mapped_files);
+				else {
+						cerr << opt_parse.help_message() << endl;
+						return EXIT_SUCCESS;
+				}
+				const string read_file_names_file(leftover_args.back());
+				vector<string> reads_files;
+				if (!read_file_names_file.empty())
+						read_filename_file(read_file_names_file.c_str(), reads_files);
+				else {
+						cerr << opt_parse.help_message() << endl;
+						return EXIT_SUCCESS;
+				}
+				/****************** END COMMAND LINE OPTIONS *****************/
     
-    vector<FileIterator<GenomicRegion> *> itrs;
-    if (VERBOSE)
-      cerr << "[MAPPED READS FILES:]" << endl;
-    for (size_t i = 0; i < mapped_files.size(); ++i) {
-      if (VERBOSE)
-	cerr << mapped_files[i] << endl;
-      itrs.push_back(new FileIterator<GenomicRegion>(mapped_files[i], BUFFER_SIZE));
-    }
+				vector<FileIterator<GenomicRegion> *> itrs;
+				if (VERBOSE)
+						cerr << "[MAPPED READS FILES:]" << endl;
+				for (size_t i = 0; i < mapped_files.size(); ++i) {
+						if (VERBOSE)
+								cerr << mapped_files[i] << endl;
+						itrs.push_back(new FileIterator<GenomicRegion>(mapped_files[i], BUFFER_SIZE));
+				}
     
-    vector<FileIterator<FASTQRecord> *> read_itrs;
-    if (VERBOSE)
-      cerr << "[FASTQ READS FILES:]" << endl;
-    for (size_t i = 0; i < reads_files.size(); ++i) {
-      if (VERBOSE)
-	cerr << reads_files[i] << endl;
-      read_itrs.push_back(new FileIterator<FASTQRecord>(reads_files[i], BUFFER_SIZE));
-    }
+				vector<FileIterator<FASTQRecord> *> read_itrs;
+				if (VERBOSE)
+						cerr << "[FASTQ READS FILES:]" << endl;
+				for (size_t i = 0; i < reads_files.size(); ++i) {
+						if (VERBOSE)
+								cerr << reads_files[i] << endl;
+						read_itrs.push_back(new FileIterator<FASTQRecord>(reads_files[i], BUFFER_SIZE));
+				}
     
-    std::priority_queue<pair<GenomicRegion, size_t>, 
-      vector<pair<GenomicRegion, size_t> >, ComparePairs> a;
-    for (size_t i = 0; i < itrs.size(); ++i)
-      a.push(make_pair(*itrs[i]->get_first(), i));
+				std::priority_queue<pair<GenomicRegion, size_t>, 
+						vector<pair<GenomicRegion, size_t> >, ComparePairs> a;
+				for (size_t i = 0; i < itrs.size(); ++i)
+						a.push(make_pair(*itrs[i]->get_first(), i));
     
-    ofstream out(map_outfile.c_str());
-    ofstream read_out(read_outfile.c_str());
+				ofstream out(map_outfile.c_str());
+				ofstream read_out(read_outfile.c_str());
 
-    vector<GenomicRegion> mapped_ties;
-    vector<FASTQRecord> reads_ties;
-    double score = std::numeric_limits<double>::max();
-    const Runif rng(random_number_seed);
+				vector<GenomicRegion> mapped_ties;
+				vector<FASTQRecord> reads_ties;
+				double score = std::numeric_limits<double>::max();
+				const Runif rng(random_number_seed);
     
-    while (!a.empty()) {
-      const size_t file_id = a.top().second;
-      if (mapped_ties.empty() || 
-	  !(mapped_ties.front() < a.top().first)) {
-	double new_score = a.top().first.get_score();
-	if (new_score < score) {
-	  new_score = a.top().first.get_score();
-	  mapped_ties.clear();
-	  reads_ties.clear();
-	}
-      }
-      else {
-	const size_t rand_idx = rng.runif(0ul, mapped_ties.size());
-	out << mapped_ties[rand_idx] << '\n';
-	read_out << reads_ties[rand_idx] << '\n';
-	mapped_ties.clear();
-	reads_ties.clear();
-	score = std::numeric_limits<double>::max();
-      }
-      mapped_ties.push_back(a.top().first);
-      reads_ties.push_back(*read_itrs[file_id]->get_first());
-      a.pop();
-      itrs[file_id]->increment_first();
-      read_itrs[file_id]->increment_first();
-      if (itrs[file_id]->first_is_good())
-	a.push(make_pair(*itrs[file_id]->get_first(), file_id));
-    }
-    const size_t rand_idx = rng.runif(0ul, mapped_ties.size());
-    out << mapped_ties[rand_idx] << '\n';
-    read_out << reads_ties[rand_idx] << '\n';
-  }
-  catch (const RMAPException &e) {
-    cerr << e.what() << endl;
-    return EXIT_FAILURE;
-  }
-  catch (std::bad_alloc &ba) {
-    cerr << "ERROR: could not allocate memory" << endl;
-    return EXIT_FAILURE;
-  }
-  return EXIT_SUCCESS;
+				while (!a.empty()) {
+						const size_t file_id = a.top().second;
+						if (mapped_ties.empty() || 
+							!(mapped_ties.front() < a.top().first)) {
+								double new_score = a.top().first.get_score();
+								if (new_score < score) {
+										new_score = a.top().first.get_score();
+										mapped_ties.clear();
+										reads_ties.clear();
+								}
+						}
+						else {
+								const size_t rand_idx = rng.runif(0ul, mapped_ties.size());
+								out << mapped_ties[rand_idx] << '\n';
+								read_out << reads_ties[rand_idx] << '\n';
+								mapped_ties.clear();
+								reads_ties.clear();
+								score = std::numeric_limits<double>::max();
+						}
+						mapped_ties.push_back(a.top().first);
+						reads_ties.push_back(*read_itrs[file_id]->get_first());
+						a.pop();
+						itrs[file_id]->increment_first();
+						read_itrs[file_id]->increment_first();
+						if (itrs[file_id]->first_is_good())
+								a.push(make_pair(*itrs[file_id]->get_first(), file_id));
+				}
+				const size_t rand_idx = rng.runif(0ul, mapped_ties.size());
+				out << mapped_ties[rand_idx] << '\n';
+				read_out << reads_ties[rand_idx] << '\n';
+		}
+		catch (const RMAPException &e) {
+				cerr << e.what() << endl;
+				return EXIT_FAILURE;
+		}
+		catch (std::bad_alloc &ba) {
+				cerr << "ERROR: could not allocate memory" << endl;
+				return EXIT_FAILURE;
+		}
+		return EXIT_SUCCESS;
 }
