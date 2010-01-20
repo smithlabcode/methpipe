fac2d540d818c786da8f8021dda941f40802585a
songqiang <qiang.song@usc.edu>
2010-01-20 00:15:43 +0000

    add options for methseg-parser allowing specify min_score and choose output of 4 colum BED file

--- bsutils/tools/methseg-parser.py
+++ bsutils/tools/methseg-parser.py
@@ -6,13 +6,24 @@
 # certain criteria. It uses stdin and stdout.
 
 import sys, string
+from optparse import OptionParser
 
-min_posterier_score = 15				# almost equivalent to domains containg more than 15 CpGs 
+opt_parser = OptionParser()
+opt_parser.add_option("-c", "--min_score", dest = "min_posterier_score", default = 20, type="float",\
+					  help="Minimum score for a selected domain")
+opt_parser.add_option("-b", "--bed_file", help="output bed file corresponding to original BED format",\
+					  default = False, action="store_true", dest="output_bed")
+(options, args) = opt_parser.parse_args()
 
-for line in open(sys.stdin):
+# min_posterier_score = 20				# almost equivalent to domains containg more than 15 CpGs 
+
+for line in sys.stdin:
 	(chrom, start, end, readName, score, strand) = line.split()
 	score = float(score)
-	if score > min_posterier_score:
-		print line
+	if score > options.min_posterier_score:
+		if options.output_bed:
+			sys.stdout.write("%s\t%s\t%s\t%s\n" % (chrom, start, end, readName)) 
+		else:
+			sys.stdout.write(line)
 
 	
