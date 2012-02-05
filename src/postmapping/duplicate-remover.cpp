9e8a2a483f8900a4b4516428e18f3148153a88f8
andrew <andrewds@usc.edu>
2012-02-05 03:05:16 +0000

    Now this program will only handle either positive strand reads or negative strand reads in a given run, because the 5-prime end is different for these. Correspondingly, the required sorted order of the input is different: chrom, start, strand, end, OR chrom, end, strand, start.

--- src/postmapping/duplicate-remover.cpp
+++ src/postmapping/duplicate-remover.cpp
@@ -44,7 +44,7 @@ same_strand(const GenomicRegion &a, const GenomicRegion &b) {
   return a.get_strand() == b.get_strand();
 }
 static inline bool
-strand_leq(const GenomicRegion &a, const GenomicRegion &b) {
+strand_less(const GenomicRegion &a, const GenomicRegion &b) {
   return a.get_strand() <= b.get_strand();
 }
 static inline bool
@@ -60,6 +60,10 @@ start_less(const GenomicRegion &a, const GenomicRegion &b) {
   return a.get_start() < b.get_start();
 }
 static inline bool
+start_leq(const GenomicRegion &a, const GenomicRegion &b) {
+  return a.get_start() <= b.get_start();
+}
+static inline bool
 same_end(const GenomicRegion &a, const GenomicRegion &b) {
   return a.get_end() == b.get_end();
 }
@@ -67,7 +71,10 @@ static inline bool
 end_less(const GenomicRegion &a, const GenomicRegion &b) {
   return a.get_end() < b.get_end();
 }
-
+static inline bool
+end_leq(const GenomicRegion &a, const GenomicRegion &b) {
+  return a.get_end() <= b.get_end();
+}
 /******************************************************************************/
 
 
@@ -89,8 +96,8 @@ private:
 	    (prev_mr.same_chrom(mr) &&
 	     (start_less(prev_mr, mr) ||
 	      (same_start(prev_mr, mr) &&
-	       (end_less(prev_mr, mr) ||
-		(same_end(prev_mr, mr) && strand_leq(prev_mr, mr)))))));
+	       (strand_less(prev_mr, mr) ||
+		(same_strand(prev_mr, mr) && end_leq(prev_mr, mr)))))));
   }
   
   static bool 
@@ -99,8 +106,8 @@ private:
 	    (prev_mr.same_chrom(mr) &&
 	     (end_less(prev_mr, mr) || 
 	      (same_end(prev_mr, mr) &&
-	       (start_less(prev_mr, mr) || 
-		(same_start(prev_mr, mr) && strand_leq(prev_mr, mr)))))));
+	       (strand_less(prev_mr, mr) ||
+		(same_strand(prev_mr, mr) && start_leq(prev_mr, mr)))))));
   }
 };
 bool OrderChecker::CHECK_SECOND_ENDS = false;
@@ -125,22 +132,18 @@ private:
 
   static bool
   the_real_test(const GenomicRegion &lhs, const GenomicRegion &rhs) {
-    if (!same_strand(lhs, rhs) || !lhs.same_chrom(rhs)) 
+    if (!same_strand(lhs, rhs) || !lhs.same_chrom(rhs))
       return false;
     
     const bool lhs_is_frag = internal_is_complete_fragment(lhs);
     const bool rhs_is_frag = internal_is_complete_fragment(rhs);
     
-    // if ((lhs_is_frag && rhs_is_frag) || (!lhs_is_frag && !rhs_is_frag))
-    if (lhs_is_frag == rhs_is_frag)
+    if (lhs_is_frag && rhs_is_frag)
       return same_start(lhs, rhs) && same_end(lhs, rhs);
     
-    if (CHECK_SECOND_ENDS)
-      return (lhs.pos_strand() && same_end(lhs, rhs)) ||
-	(lhs.neg_strand() && same_start(lhs, rhs));
-    
-    return (lhs.pos_strand() && same_start(lhs, rhs)) ||
-      (lhs.neg_strand() && same_end(lhs, rhs));
+    return CHECK_SECOND_ENDS ?
+      (lhs.neg_strand() && same_end(lhs, rhs)) :
+      (lhs.pos_strand() && same_start(lhs, rhs));
   }
   static bool
   internal_is_complete_fragment(const GenomicRegion &mr) {
@@ -150,61 +153,60 @@ private:
     return lexicographical_compare(name.begin(), name.begin() + label_len,
 				   label, label + label_len);
   }
-
+  
 };
 bool DuplicateFragmentTester::CHECK_SECOND_ENDS = false;
-
-
-class LessMismatchCmp: public std::binary_function<MappedRead, MappedRead, bool>
-{
-public:
-  bool operator()(const MappedRead &a, const MappedRead &b) const
-  {
-    return ((a.r.get_width() - count(a.seq.begin(), a.seq.end(), 'N') - a.r.get_score()) > 
-	    (b.r.get_width() - count(b.seq.begin(), b.seq.end(), 'N') - b.r.get_score()));
-    // return a.r.get_score() < b.r.get_score();
-  }
-};
-
-class SameMismatchCmp: public std::binary_function<MappedRead, MappedRead, bool>
-{
-public:
-  bool operator()(const MappedRead &a, const MappedRead &b) const
-  {
-    return ((a.r.get_width() - count(a.seq.begin(), a.seq.end(), 'N') - a.r.get_score()) ==
-	    (b.r.get_width() - count(b.seq.begin(), b.seq.end(), 'N') - b.r.get_score()));
-  }
-};
-
-static size_t
-get_representative_read(vector<MappedRead> &candidates) {
-  static const Runif rng(time(NULL) + getpid());
-  vector<MappedRead>::iterator iter =
-    std::partition(candidates.begin(), candidates.end(), 
-		   &DuplicateFragmentTester::is_complete_fragment);
-  iter = ((iter != candidates.begin()) ? iter : candidates.end());
-  const vector<MappedRead>::const_iterator min_mismatch_iter =
-      std::min_element(candidates.begin(), iter, LessMismatchCmp());
-  iter = std::partition(candidates.begin(), iter,
-                        bind2nd(SameMismatchCmp(), *min_mismatch_iter));
-  return rng.runif(static_cast<size_t>(0), static_cast<size_t>(iter - candidates.begin()));
-  //return rng.runif(static_cast<size_t>(0), iter - candidates.begin());//////////
-}
-
 
 static inline size_t
 count_good_bases(const MappedRead &mr) {
   return mr.r.get_width() - count(mr.seq.begin(), mr.seq.end(), 'N');
 }
 
+class LessMismatchCmp: public std::binary_function<MappedRead, MappedRead, bool> {
+public:
+  bool operator()(const MappedRead &a, const MappedRead &b) const {
+    return ((count_good_bases(a) - a.r.get_score()) > 
+	    (count_good_bases(b) - b.r.get_score()));
+  }
+};
+
+
+class SameMismatchCmp: public std::binary_function<MappedRead, MappedRead, bool> {
+public:
+  bool operator()(const MappedRead &a, const MappedRead &b) const {
+    return ((count_good_bases(a) - a.r.get_score()) ==
+	    (count_good_bases(b) - b.r.get_score()));
+  }
+};
+
+
+static size_t
+get_representative_read(const Runif &rng, vector<MappedRead> &candidates) {
+  vector<MappedRead>::iterator iter =
+    std::partition(candidates.begin(), candidates.end(), 
+		   &DuplicateFragmentTester::is_complete_fragment);
+  iter = ((iter != candidates.begin()) ? iter : candidates.end());
+  const vector<MappedRead>::const_iterator min_mismatch_iter =
+    std::min_element(candidates.begin(), iter, LessMismatchCmp());
+  iter = std::partition(candidates.begin(), iter,
+                        bind2nd(SameMismatchCmp(), *min_mismatch_iter));
+  return rng.runif(static_cast<size_t>(0),
+		   static_cast<size_t>(iter - candidates.begin()));
+}
+
 
 static void
 remove_duplicates(const string &infile, const string &outfile, 
 		  size_t &total_reads, size_t &unique_reads,
+		  size_t &total_reads_pos, size_t &uniq_reads_pos,
 		  size_t &total_bases_in, size_t &good_bases_out) {
   
+  static const Runif rng(time(0) + getpid());
+  
+  srand(time(0) + getpid());
+
   DuplicateFragmentTester dft;
-  OrderChecker oc;
+  OrderChecker order_check;
   
   std::ofstream of;
   if (!outfile.empty()) of.open(outfile.c_str());
@@ -219,31 +221,34 @@ remove_duplicates(const string &infile, const string &outfile,
     throw SMITHLABException("mapped read file seems empty: " + infile);
   
   ++total_reads;
+  total_reads_pos += mr.r.pos_strand();
   
   vector<MappedRead> candidates;
   candidates.push_back(mr);
   
   while (in >> mr) {
     ++total_reads;
+    total_reads_pos += mr.r.pos_strand();
     total_bases_in += mr.r.get_width();
     
-    if (!oc(candidates.back(), mr))
+    if (!order_check(candidates.back(), mr))
       throw SMITHLABException("NOT SORTED:\n" + candidates.back().r.tostring() + 
-			  "\n" + mr.r.tostring());
+			      "\n" + mr.r.tostring());
     
     if (!dft(candidates.front(), mr)) {
-      const size_t rep_idx = get_representative_read(candidates);
+      const size_t rep_idx = get_representative_read(rng, candidates);
       out << candidates[rep_idx] << '\n';
       ++unique_reads;
+      uniq_reads_pos += candidates[rep_idx].r.pos_strand();
       good_bases_out += count_good_bases(candidates[rep_idx]);
       candidates.clear();
     }
-    
     candidates.push_back(mr);
   }
-  const size_t rep_idx = get_representative_read(candidates);
+  const size_t rep_idx = get_representative_read(rng, candidates);
   out << candidates[rep_idx] << '\n';
   ++unique_reads;
+  uniq_reads_pos += candidates[rep_idx].r.pos_strand();
   good_bases_out += count_good_bases(candidates[rep_idx]);
 }
 
@@ -259,7 +264,7 @@ int main(int argc, const char **argv) {
     bool INPUT_FROM_STDIN = false;
     
     /****************** COMMAND LINE OPTIONS ********************/
-    OptionParser opt_parse(argv[0], "program to remove duplicate reads from "
+    OptionParser opt_parse(strip_path(argv[0]), "program to remove duplicate reads from "
 			   "sorted mapped reads", "<mapped-reads>");
     opt_parse.add_opt("output", 'o', "output file for unique reads",
 		      false, outfile);
@@ -296,13 +301,16 @@ int main(int argc, const char **argv) {
       infile = leftover_args.front();
     /****************** END COMMAND LINE OPTIONS *****************/
     
-    size_t total_reads = 0, unique_reads = 0, total_bases_in = 0, good_bases_out = 0;
+    size_t total_reads = 0, unique_reads = 0, 
+      total_reads_pos = 0, uniq_reads_pos = 0,
+      total_bases_in = 0, good_bases_out = 0;
 
     OrderChecker::set_comparison_mode(CHECK_SECOND_ENDS);
     DuplicateFragmentTester::set_comparison_mode(CHECK_SECOND_ENDS);
     
-    remove_duplicates(infile, outfile, total_reads, 
-		      unique_reads, total_bases_in, good_bases_out);
+    remove_duplicates(infile, outfile, total_reads, unique_reads, 
+		      total_reads_pos, uniq_reads_pos,
+		      total_bases_in, good_bases_out);
     
     if (!statfile.empty() || VERBOSE) {
       std::ofstream of;
@@ -310,6 +318,10 @@ int main(int argc, const char **argv) {
       std::ostream out(statfile.empty() ? std::clog.rdbuf() : of.rdbuf());
       out << "TOTAL READS:\t" << total_reads << endl
 	  << "UNIQUE READS:\t" << unique_reads << endl
+	  << "TOTAL POS:\t" << total_reads_pos << endl
+	  << "UNIQUE POS:\t" << uniq_reads_pos << endl
+	  << "TOTAL NEG:\t" << total_reads - total_reads_pos << endl
+	  << "UNIQUE NEG:\t" << unique_reads - uniq_reads_pos << endl
 	  << "ALL BASES IN:\t" << total_bases_in << endl
 	  << "GOOD BASES OUT:\t" << good_bases_out << endl;
     }
