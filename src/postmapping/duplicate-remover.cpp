f1fe39adb795ad82f9219b97f8499f09d56abd20
songqiang <qiang.song@usc.edu>
2013-08-06 00:36:55 +0000

    add unistd header file to use getpid

--- src/postmapping/duplicate-remover.cpp
+++ src/postmapping/duplicate-remover.cpp
@@ -19,6 +19,9 @@
  *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <sys/types.h>
+#include <unistd.h>
+
 #include <string>
 #include <vector>
 #include <iostream>
