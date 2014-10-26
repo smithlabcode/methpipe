8108e4c385974a3e5d251c87bef66383888f9f70
Egor <egor.dolzhenko@gmail.com>
2014-10-25 23:24:52 -0700

    Merged the program to combine significant loci into radmeth.

--- src/radmeth/radmeth.cpp
+++ src/radmeth/radmeth.cpp
@@ -36,6 +36,7 @@
 // Local headers.
 #include "regression.hpp"
 #include "combine_pvals.hpp"
+#include "merge.hpp"
 
 using std::string;
 using std::vector;
@@ -392,6 +393,51 @@ main(int argc, const char **argv) {
       //TODO: Check that the regions do not overlap & sorted
 
 
+    } else if (command_name == "merge") {
+      /* FILES */
+      string outfile;
+      string bin_spec = "1:200:25";
+      double cutoff = 0.01;
+
+      /****************** GET COMMAND LINE ARGUMENTS ***************************/
+      OptionParser opt_parse("dmrs", "a program to merge significantly "
+                             "differentially methylated CpGs into DMRs",
+                             "<bed-file-in-wand-format>");
+      opt_parse.add_opt("output", 'o', "Name of output file (default: stdout)",
+            false , outfile);
+      opt_parse.add_opt("cutoff", 'p', "P-value cutoff (default: 0.01)",
+            false , cutoff);
+      vector<string> leftover_args;
+      opt_parse.parse(argc - 1, argv + 1, leftover_args);
+      if (argc == 1 || opt_parse.help_requested()) {
+        cerr << opt_parse.help_message() << endl;
+        return EXIT_SUCCESS;
+      }
+      if (opt_parse.about_requested()) {
+        cerr << opt_parse.about_message() << endl;
+        return EXIT_SUCCESS;
+      }
+      if (opt_parse.option_missing()) {
+        cerr << opt_parse.option_missing_message() << endl;
+        return EXIT_SUCCESS;
+      }
+      if (leftover_args.size() != 1) {
+        cerr << opt_parse.help_message() << endl;
+        return EXIT_SUCCESS;
+      }
+      const string bed_filename = leftover_args.front();
+      /*************************************************************************/
+
+      std::ofstream of;
+      if (!outfile.empty()) of.open(outfile.c_str());
+      std::ostream out(outfile.empty() ? std::cout.rdbuf() : of.rdbuf());
+
+      std::ifstream bed_file(bed_filename.c_str());
+
+      if (!bed_file)
+        throw "could not open file: " + bed_filename;
+
+      merge(bed_file, out, cutoff);
     }
     /****************** END COMMAND LINE OPTIONS *****************/
 
