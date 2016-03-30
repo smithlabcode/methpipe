9c984e621c5752545a03c6b37db057708379e690
andrewdavidsmith <andrew@smithlabresearch.org>
2016-03-30 11:57:54 -0700

    Adding me as an author. It seems like I wrote a lot of this source file.

--- src/utils/merge-methcounts.cpp
+++ src/utils/merge-methcounts.cpp
@@ -3,7 +3,7 @@
  *    Copyright (C) 2011-2014 University of Southern California and
  *                            Andrew D. Smith
  *
- *    Authors: Benjamin E Decato and Meng Zhou
+ *    Authors: Benjamin E Decato, Meng Zhou and Andrew D Smith
  *
  *    This program is free software: you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by
