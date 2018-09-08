77d7728e6c52592a71706834d86f5e69183ef082
andrewdavidsmith <andrewds@usc.edu>
2018-09-07 21:18:27 -0700

    Removed some code that was commented out

--- src/analysis/levels.cpp
+++ src/analysis/levels.cpp
@@ -52,194 +52,6 @@ using std::endl;
 using std::to_string;
 using std::runtime_error;
 
-// struct LevelsSite {
-//   string chrom;
-//   size_t pos;
-//   string strand;
-//   string context;
-//   double meth;
-//   size_t n_reads;
-
-//   size_t n_meth() const {return std::round(meth*n_reads);}
-
-//   void add(const LevelsSite &other) {
-//     if (!is_mutated() && other.is_mutated())
-//       context += 'x';
-//     // ADS: order matters below as n_reads update invalidates n_meth()
-//     // function until meth has been updated
-//     const size_t total_c_reads = n_meth() + other.n_meth();
-//     n_reads += other.n_reads;
-//     meth = static_cast<double>(total_c_reads)/n_reads;
-//   }
-
-//   // ADS: function below has redundant check for is_cpg, which is
-//   // expensive and might be ok to remove
-//   bool is_mate_of(const LevelsSite &first) {
-//     return (first.pos + 1 == pos && first.is_cpg() && is_cpg() &&
-//             first.strand == "+" && strand == "-");
-//   }
-//   ////////////////////////////////////////////////////////////////////////
-//   /////  Functions below test the type of site. These are CpG, CHH, and
-//   /////  CHG divided into two kinds: CCG and CXG, the former including a
-//   /////  CpG within. Also included is a function that tests if a site
-//   /////  has a mutation.
-//   /////  WARNING: None of these functions test for the length of their
-//   /////  argument string, which could cause problems.
-//   ////////////////////////////////////////////////////////////////////////
-//   bool is_cpg() const {
-//     return (context[0] == 'C' && context[1] == 'p' && context[2] == 'G');
-//   }
-//   bool is_chh() const {
-//     return (context[0] == 'C' && context[1] == 'H' && context[2] == 'H');
-//   }
-//   bool is_ccg() const {
-//     return (context[0] == 'C' && context[1] == 'C' && context[2] == 'G');
-//   }
-//   bool is_cxg() const {
-//     return (context[0] == 'C' && context[1] == 'X' && context[2] == 'G');
-//   }
-//   bool is_mutated() const {
-//     return context[3] == 'x';
-//   }
-// };
-
-
-// struct LevelsCounter {
-//   string context;
-//   size_t total_sites;
-//   size_t sites_covered;
-//   size_t max_depth;
-//   size_t mutations;
-//   size_t total_c, total_t;
-//   size_t called_meth, called_unmeth;
-//   double mean_agg;
-//   LevelsCounter(const string &c) :
-//     context(c), total_sites(0), sites_covered(0), max_depth(0),
-//     mutations(0), total_c(0), total_t(0),
-//     called_meth(0), called_unmeth(0),
-//     mean_agg(0.0) {}
-
-//   void update(const LevelsSite &s) {
-//     if (s.is_mutated()) {
-//       ++mutations;
-//     }
-//     else if (s.n_reads > 0) {
-//       ++sites_covered;
-//       max_depth = std::max(max_depth, s.n_reads);
-//       total_c += s.n_meth();
-//       total_t += s.n_reads - s.n_meth();
-//       mean_agg += s.meth;
-//       double lower = 0.0, upper = 0.0;
-//       wilson_ci_for_binomial(alpha, s.n_reads, s.meth, lower, upper);
-//       called_meth += (lower > 0.5);
-//       called_unmeth += (upper < 0.5);
-//     }
-//     ++total_sites;
-//   }
-
-//   size_t coverage() const {return total_c + total_t;}
-//   size_t total_called() const {return called_meth + called_unmeth;}
-
-//   double mean_meth_weighted() const {
-//     return static_cast<double>(total_c)/coverage();
-//   }
-//   double fractional_meth() const {
-//     return static_cast<double>(called_meth)/total_called();
-//   }
-//   double mean_meth() const {
-//     return mean_agg/sites_covered;
-//   }
-
-//   string tostring() const {
-//     static const string indent = string(2, ' ');
-//     const bool good = (sites_covered != 0);
-//     std::ostringstream oss;
-//     // directly counted values
-//     oss << context + ":\n"
-//         << indent << "total_sites: " << total_sites << '\n'
-//         << indent << "sites_covered: " << sites_covered << '\n'
-//         << indent << "total_c: " << total_c << '\n'
-//         << indent << "total_t: " << total_t << '\n'
-//         << indent << "max_depth: " << max_depth << '\n'
-//         << indent << "mutations: " << mutations << '\n'
-//         << indent << "called_meth: " << called_meth << '\n'
-//         << indent << "called_unmeth: " << called_unmeth << '\n'
-//         << indent << "mean_agg: " << mean_agg << '\n';
-
-//     // derived values
-//     oss << indent << "coverage: " << coverage() << '\n'
-//         << indent << "sites_covered_fraction: "
-//         << static_cast<double>(sites_covered)/total_sites << '\n'
-//         << indent << "mean_depth: "
-//         << static_cast<double>(coverage())/total_sites << '\n'
-//         << indent << "mean_depth_covered: "
-//         << static_cast<double>(coverage())/sites_covered << '\n'
-//         << indent << "mean_meth: "
-//         << (good ? to_string(mean_meth()) : "NA")  << '\n'
-//         << indent << "mean_meth_weighted: "
-//         << (good ? to_string(mean_meth_weighted()) : "NA") << '\n'
-//         << indent << "fractional_meth: "
-//         << (good ? to_string(fractional_meth()) : "NA");
-//     return oss.str();
-//   }
-
-//   static double alpha;
-// };
-
-// double LevelsCounter::alpha = 0.95;
-
-// std::ostream &
-// operator<<(std::ostream &out, const LevelsCounter &cs) {
-//   return out << cs.tostring();
-// }
-
-// static void
-// check_label(const string &observed, const string expected) {
-//   if (observed != expected)
-//     throw runtime_error("bad levels format [" + observed + "," + expected + "]");
-// }
-
-// std::istream &
-// operator>>(std::istream &in, LevelsCounter &cs) {
-//   in >> cs.context; // get the context
-//   cs.context = cs.context.substr(0, cs.context.find_first_of(":"));
-
-//   string label;
-//   in >> label >> cs.total_sites; // the total sites
-//   check_label(label, "total_sites:");
-
-//   in >> label >> cs.sites_covered; // the sites covered
-//   check_label(label, "sites_covered:");
-
-//   in >> label >> cs.total_c; // the total c
-//   check_label(label, "total_c:");
-
-//   in >> label >> cs.total_t; // the total t
-//   check_label(label, "total_t:");
-
-//   in >> label >> cs.max_depth; // the max depth
-//   check_label(label, "max_depth:");
-
-//   in >> label >> cs.mutations; // the number of mutations
-//   check_label(label, "mutations:");
-
-//   in >> label >> cs.called_meth; // the number of sites called methylated
-//   check_label(label, "called_meth:");
-
-//   in >> label >> cs.called_unmeth; // the number of sites called unmethylated
-//   check_label(label, "called_unmeth:");
-
-//   in >> label >> cs.mean_agg; // the mean aggregate
-//   check_label(label, "mean_agg:");
-
-//   return in;
-// }
-
-// static std::istream &
-// get_meth_unmeth(std::istream &in, LevelsSite &site) {
-//   return methpipe::read_site(in, site.chrom, site.pos, site.strand,
-//                              site.context, site.meth, site.n_reads);
-// }
 
 int
 main(int argc, const char **argv) {
@@ -293,7 +105,7 @@ main(int argc, const char **argv) {
     MSite site, prev_site;
     size_t chrom_count = 0;
 
-    while (in >> site) { //get_meth_unmeth(in, site)) {
+    while (in >> site) {
 
       if (site.chrom != prev_site.chrom) {
         ++chrom_count;
@@ -308,14 +120,10 @@ main(int argc, const char **argv) {
           cpg_symmetric.update(site);
         }
       }
-      else if (site.is_chh())
-        chh.update(site);
-      else if (site.is_ccg())
-        ccg.update(site);
-      else if (site.is_cxg())
-        cxg.update(site);
-      else
-        throw runtime_error("bad site context: " + site.context);
+      else if (site.is_chh()) chh.update(site);
+      else if (site.is_ccg()) ccg.update(site);
+      else if (site.is_cxg()) cxg.update(site);
+      else throw runtime_error("bad site context: " + site.context);
 
       cytosines.update(site);
 
@@ -326,8 +134,7 @@ main(int argc, const char **argv) {
     if (!outfile.empty()) of.open(outfile.c_str());
     std::ostream out(outfile.empty() ? std::cout.rdbuf() : of.rdbuf());
 
-    out << "number_of_chromosomes: " << chrom_count << endl
-        << cytosines << endl
+    out << cytosines << endl
         << cpg << endl
         << cpg_symmetric << endl
         << chh << endl
