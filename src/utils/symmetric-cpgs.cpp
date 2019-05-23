43e151eb0531852588810cbd0c196a7c3c947cf3
Andrew D Smith <andrewds@usc.edu>
2019-05-23 11:07:45 -0700

    symmetric-cpgs fixed copyright block

--- src/utils/symmetric-cpgs.cpp
+++ src/utils/symmetric-cpgs.cpp
@@ -1,24 +1,20 @@
-/*
- *    symmetric-cpgs: extract the CpG sites from a methcounts output
- *    file and produce a new one with the CpGs treated unstranded.
+/* symmetric-cpgs: extract the CpG sites from a methcounts output
+ * file and produce a new one with the CpGs treated unstranded.
  *
- *    Copyright (C) 2014 University of Southern California and
- *                       Andrew D. Smith
+ * Copyright (C) 2014 University of Southern California and
+ *                    Andrew D. Smith
  *
- *    Authors: Andrew D. Smith
+ * Authors: Andrew D. Smith
  *
- *    This program is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
- *
- *    You should have received a copy of the GNU General Public License
- *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
  */
 
 #include <string>
