7b1e16ce0386e6a150ae58b50e0c01f0881e3286
Andrew D Smith <andrewds@usc.edu>
2019-05-23 11:18:09 -0700

    merge-methcounts: copyright block indentation

--- src/utils/merge-methcounts.cpp
+++ src/utils/merge-methcounts.cpp
@@ -1,22 +1,19 @@
-/*    merge-methcounts: a program for merging methcounts files
+/* merge-methcounts: a program for merging methcounts files
  *
- *    Copyright (C) 2011-2019 University of Southern California and
- *                            Andrew D. Smith
+ * Copyright (C) 2011-2019 University of Southern California and
+ *                         Andrew D. Smith
  *
- *    Authors: Benjamin E Decato, Meng Zhou and Andrew D Smith
+ * Authors: Benjamin E Decato, Meng Zhou and Andrew D Smith
  *
- *    This program is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
- *
- *    You should have received a copy of the GNU General Public License
- *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
  */
 
 #include <cmath>
@@ -124,6 +121,7 @@ collect_sites_to_print(const vector<MSite> &sites, const vector<bool> &outdated,
 
   return min_site_idx;
 }
+
 
 static void
 write_line_for_tabular(std::ostream &out,
