536ea3ddba9b32059043bb8f2ceffea83f6b4035
Andrew D Smith <andrewds@usc.edu>
2019-08-10 15:29:51 -0700

    No longer pre-loads all sites. Updated for gz format output. Cleaned up and simplified code related to reading and writing sites.

--- src/radmeth/methdiff.cpp
+++ src/radmeth/methdiff.cpp
@@ -1,8 +1,8 @@
 /* methdiff: Computes probability that individual CpGs have higher
- *           methylation in file 1 than 2
+ *           methylation in file A than in file B, where files A and B
+ *           are specified on the command line.
  *
- * Copyright (C) 2011 University of Southern California
- *                    Andrew D Smith
+ * Copyright (C) 2011-2019 Andrew D Smith
  *
  * Author: Andrew D. Smith
  *
@@ -11,15 +11,10 @@
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * This is distributed in the hope that it will be useful,
+ * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this software; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
  */
 
 #include <cmath>
@@ -44,6 +39,7 @@ using std::endl;
 using std::cerr;
 using std::pair;
 using std::runtime_error;
+using std::min;
 
 using std::ostream_iterator;
 using std::ofstream;
@@ -57,9 +53,6 @@ log_sum_log(const double p, const double q) {
   return larger + log(1.0 + exp(smaller - larger));
 }
 
-////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////
 
 static double
 log_hyper_g_greater(size_t meth_a, size_t unmeth_a,
@@ -72,8 +65,8 @@ log_hyper_g_greater(size_t meth_a, size_t unmeth_a,
 
 
 static double
-test_greater_population(size_t meth_a, size_t unmeth_a,
-                        size_t meth_b, size_t unmeth_b) {
+test_greater_population(const size_t meth_a, const size_t unmeth_a,
+                        const size_t meth_b, const size_t unmeth_b) {
   double p = 0;
   for (size_t k = (meth_b > unmeth_a) ? meth_b - unmeth_a : 0; k < meth_b; ++k)
     p = log_sum_log(p, log_hyper_g_greater(meth_a, unmeth_a,
@@ -82,50 +75,66 @@ test_greater_population(size_t meth_a, size_t unmeth_a,
 }
 
 
-////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////
-
-static void
-load_cpgs(const string &cpgs_file,
-          vector<MSite> &cpgs,
-          vector<pair<double, double> > &meth,
-          vector<size_t> &reads) {
-
-  igzfstream in(cpgs_file);
-  if (!in)
-    throw runtime_error("failed opening file: " + cpgs_file);
-
-  MSite the_site;
-  while (in >> the_site) {
-    cpgs.push_back(the_site);
-    reads.push_back(the_site.n_reads);
-    const double m = reads.back()*the_site.meth;
-    meth.push_back(std::make_pair(m, reads.back() - m));
-  }
-}
-
-
 template <class T> T&
-write_methdiff_site(T &out, const std::string &chrom,
-                    const size_t pos, const std::string &strand,
-                    const std::string &seq, const double diffscore,
-                    const size_t meth_a, const size_t unmeth_a,
-                    const size_t meth_b, const size_t unmeth_b) {
+write_methdiff_site(T &out,
+                    const MSite &a, const MSite &b, const double diffscore) {
+
+  MSite c(a);
+  c.n_reads = a.n_meth();
+  c.meth = diffscore;
 
   std::ostringstream oss;
-  oss << chrom << "\t" << pos << "\t" << strand << "\t" << seq << "\t"
-      << diffscore << "\t" << meth_a << "\t" << unmeth_a << "\t"
-      << meth_b << "\t" << unmeth_b << '\n';
+  oss << c;
+
+  oss << '\t' << a.n_unmeth() // a.n_meth() already output with 'c'
+      << '\t' << b.n_meth()
+      << '\t' << b.n_unmeth() << endl;
+
   out << oss.str();
   return out;
 }
 
 
 static bool
-same_chrom(const MSite &a, const MSite &b) {
-  return a.chrom == b.chrom;
+same_chrom_and_pos(const MSite &a, const MSite &b) {
+  return a.chrom == b.chrom && a.pos == b.pos;
 }
+
+
+template <class T>
+static void
+process_sites(const bool VERBOSE, igzfstream &in_a, igzfstream &in_b,
+              const bool allow_uncovered, const double pseudocount, T &out) {
+
+  MSite a, b;
+  string prev_chrom;
+  in_b >> b; // load first site for "b"
+  while (in_a >> a) {
+
+    if (VERBOSE && a.chrom != prev_chrom)
+      cerr << "[processing: " << a.chrom << "]" << endl;
+
+    while (in_b && b < a) in_b >> b; // find appropriate "b" site
+
+    if (same_chrom_and_pos(a, b)) {
+
+      if (allow_uncovered || min(a.n_reads, b.n_reads) > 0) {
+
+        const size_t meth_a = a.n_meth() + pseudocount;
+        const size_t unmeth_a = a.n_unmeth() + pseudocount;
+        const size_t meth_b = b.n_meth() + pseudocount;
+        const size_t unmeth_b = b.n_unmeth() + pseudocount;
+
+        const double diffscore = test_greater_population(meth_b, unmeth_b,
+                                                         meth_a, unmeth_a);
+
+        write_methdiff_site(out, a, b, diffscore);
+      }
+    }
+    swap(prev_chrom, a.chrom);
+  }
+}
+
 
 int
 main(int argc, const char **argv) {
@@ -136,21 +145,20 @@ main(int argc, const char **argv) {
     size_t pseudocount = 1;
 
     // run mode flags
-    bool ONLY_HIGH_COVERAGE_LOCI = false;
+    bool allow_uncovered = true;
     bool VERBOSE = false;
 
     /****************** COMMAND LINE OPTIONS ********************/
     OptionParser opt_parse(strip_path(argv[0]),
-                           "compute probability a "
-                           "CpG has higher methylation in file A than B",
-                           "<meth-file-A> <meth-file-B>");
+                           "compute probability that site "
+                           "has higher methylation in file A than B",
+                           "<methcounts-A> <methcounts-B>");
     opt_parse.add_opt("pseudo", 'p', "pseudocount (default: 1)",
                       false, pseudocount);
     opt_parse.add_opt("nonzero-only", 'A',
-                      "output only sites with high coveage in both samples)",
-                      false, ONLY_HIGH_COVERAGE_LOCI);
-    opt_parse.add_opt("out", 'o', "output file (BED format)",
-                      false, outfile);
+                      "process only sites with coveage in both samples",
+                      false, allow_uncovered);
+    opt_parse.add_opt("out", 'o', "output file", true, outfile);
     opt_parse.add_opt("verbose", 'v', "print more run info", false, VERBOSE);
     vector<string> leftover_args;
     opt_parse.parse(argc, argv, leftover_args);
@@ -175,73 +183,28 @@ main(int argc, const char **argv) {
     const string cpgs_file_b = leftover_args[1];
     /****************** END COMMAND LINE OPTIONS *****************/
 
-    vector<MSite> cpgs_a;
-    vector<pair<double, double> > meth_unmeth_a;
-    vector<size_t> reads_a;
     if (VERBOSE)
-      cerr << "[READING METH FILE] " << cpgs_file_a << endl;
-    load_cpgs(cpgs_file_a, cpgs_a, meth_unmeth_a, reads_a);
-    if (VERBOSE)
-      cerr << "TOTAL CPGS: " << cpgs_a.size() << endl;
-
-    vector<MSite> cpgs_b;
-    vector<pair<double, double> > meth_unmeth_b;
-    vector<size_t> reads_b;
-    if (VERBOSE)
-      cerr << "[READING METH FILE] " << cpgs_file_b << endl;
-    load_cpgs(cpgs_file_b, cpgs_b, meth_unmeth_b, reads_b);
+      cerr << "[opening methcounts file: " << cpgs_file_a << "]" << endl;
+    igzfstream in_a(cpgs_file_a);
+    if (!in_a)
+      throw runtime_error("cannot open file: " + cpgs_file_a);
 
     if (VERBOSE)
-      cerr << "TOTAL CPGS: " << cpgs_b.size() << endl;
+      cerr << "[opening methcounts file: " << cpgs_file_b << "]" << endl;
+    igzfstream in_b(cpgs_file_b);
+    if (!in_b)
+      throw runtime_error("cannot open file: " + cpgs_file_b);
 
-    std::ofstream of;
-    if (!outfile.empty()) of.open(outfile);
-    std::ostream out(outfile.empty() ? std::cout.rdbuf() : of.rdbuf());
+    if (outfile.empty() || !has_gz_ext(outfile)) {
+      std::ofstream of;
+      if (!outfile.empty()) of.open(outfile);
+      std::ostream out(outfile.empty() ? cout.rdbuf() : of.rdbuf());
 
-    size_t j = 0;
-    for (size_t i = 0; i < cpgs_a.size(); ++i) {
-      const size_t meth_a = meth_unmeth_a[i].first;
-      const size_t unmeth_a = meth_unmeth_a[i].second;
-
-      if (VERBOSE && (i == 0 || !same_chrom(cpgs_a[i - 1], cpgs_a[i])))
-        cerr << "[PROCESSING] " << cpgs_a[i].chrom << endl;
-
-      while (j < cpgs_b.size() && cpgs_b[j] < cpgs_a[i]) ++j;
-
-      if (same_chrom(cpgs_a[i], cpgs_b[j]) && cpgs_a[i].pos == cpgs_b[j].pos) {
-
-        const size_t meth_b = meth_unmeth_b[j].first;
-        const size_t unmeth_b = meth_unmeth_b[j].second;
-
-        if (meth_a + unmeth_a > 0.0 && meth_b + unmeth_b > 0.0) {
-          const double diffscore = test_greater_population(meth_b + pseudocount,
-                                                           unmeth_b + pseudocount,
-                                                           meth_a + pseudocount,
-                                                           unmeth_a + pseudocount);
-
-          const string name(cpgs_a[i].context);
-          write_methdiff_site(out, cpgs_a[i].chrom, cpgs_a[i].pos,
-                              string(1, cpgs_a[i].strand),
-                              name.substr(0, name.find_first_of(':')),
-                              diffscore, meth_a, unmeth_a,
-                              meth_b, unmeth_b);
-        }
-        else if (!ONLY_HIGH_COVERAGE_LOCI) {
-          const double diffscore =
-            test_greater_population(meth_b + pseudocount,
-                                    unmeth_b + pseudocount,
-                                    meth_a + pseudocount,
-                                    unmeth_a + pseudocount);
-
-          const string name(cpgs_a[i].context);
-          write_methdiff_site(out, cpgs_a[i].chrom,
-                              cpgs_a[i].pos,
-                              string(1, cpgs_a[i].strand),
-                              name.substr(0, name.find_first_of(':')),
-                              diffscore, meth_a, unmeth_a,
-                              meth_b, unmeth_b);
-        }
-      }
+      process_sites(VERBOSE, in_a, in_b, allow_uncovered, pseudocount, out);
+    }
+    else {
+      ogzfstream out(outfile);
+      process_sites(VERBOSE, in_a, in_b, allow_uncovered, pseudocount, out);
     }
   }
   catch (runtime_error &e) {
